0f43ca7066e785ab27c9a4b65b91623cb4e4a698
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-11-17 09:55:35 -0200

    tries set lgnd uprigth

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -43,7 +43,8 @@ function montaLegendaEstados(){
 	divLegenda.style.display = 'inline-block';
 	divLegenda.style.width = '15%';
 	divLegenda.style.position = 'absolute';
-	divLegenda.style.left ='10px';
+	divLegenda.style.right ='10px';
+	divLegenda.style.top ='10px';
 //	divLegenda.style.padding = '5px';
 	divLegenda.innerHTML = "Legenda";
 	document.getElementsByClassName("painel-grafico-primeiro")[0].appendChild(divLegenda);
