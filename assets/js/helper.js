c9ff401c67f4f4ce8e5c2acc36104e2357b39d7b
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-11-22 00:18:50 -0200

    --retry diff select

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -307,7 +307,7 @@ function constroiCirculosEstadosOriginal(xScale,yScale,rScale){
 //desenha os circulos - grafico estados
 function desenhaCirculosEstadoOriginal(xScale,yScale,rScale){
 
-	d3.select(".chart-estados").selectAll("#circuloEstado")
+	d3.select(".chart-estados")//.selectAll("#circuloEstado")
     .data(dataset)
 		.enter()
 		.append("circle")
