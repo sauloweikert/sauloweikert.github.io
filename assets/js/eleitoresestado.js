3de0e14e4497d4d7329a148df19bb984dfba0094
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-11-24 00:54:09 -0200

    try largura

--- assets/js/eleitoresestado.js
+++ assets/js/eleitoresestado.js
@@ -21,10 +21,15 @@ function fazGraficoEleitores(opcao){
 
 
   /*Cria objeto svg e o objeto de grupo g para manipulação do DOM.*/
+
+ var largura = 800;
+
+  svg.attr("width",largura);
+
   var svg = d3.select("svg"),
       margin = {top: 20, right: 20, bottom: 30, left: 40},
       //margin = {top: 0, right: 0, bottom: 0, left: 0},
-      width = svg.attr("width",1100) - margin.left - margin.right - 150,
+      width = +largura - margin.left - margin.right - 150,
       height = svg.attr("height",500) - margin.top - margin.bottom-20,
       g = svg.append("g").attr("transform", "translate(0,0)");
 
