eb1411de8c711ff6f51032f77d4a6ac0719a141d
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-11-25 19:49:18 -0200

    try set min xscale as half min value

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -358,7 +358,7 @@ function atualizaEscalaRaioEstados(data,rScale){
 }
 
 function atualizaEscalaX(data,padding,w,xScale){
-		 xScale.domain([d3.min(data, function(d) { return (d.fem)/(d.total); })-0.001,
+		 xScale.domain([d3.min(data, function(d) { return (d.fem)/(2*(d.total)); }),
               d3.max(data, function(d) { return (d.fem)/(d.total); })])
 		 .range([padding, w-padding]);
 		return;
